1649c6a1548d811b4ad4569b5be70f1082529fe6
Aareon <askully13@gmail.com>
2017-04-29 14:26:00 -0700

    Attempt to fix /logos not installing

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     name='lemongrab',
     version='0.1dev',
     author = "Aareon <https://github.com/Aareon>",
-    packages=['lemongrab',],
+    packages=['lemongrab','logos'],
     license='MIT License',
     long_description=open('README.md').read(),
     install_requires=[
