c0c977248ba07fa3068523588b95cda444504ad5
Aareon Sullivan <askully13@gmail.com>
2017-10-01 18:42:31 -0700

    Fix some weird brand fomatting on some machines?

--- lemongrab/utils/cpu.py
+++ lemongrab/utils/cpu.py
@@ -6,8 +6,15 @@ def cpu():
     hz_actual = cpus.get("hz_actual_raw", "")[0]
     hz_advertised = cpus.get("hz_advertised_raw", "")[0]
 
-    cpu_brand = cpus.get("brand").strip("(R)").split(" ")
-    new_cpu_brand = "".join(item + " " for item in cpu_brand if item != " ").rstrip(" ")
+    cpu_brand = cpus.get("brand").strip("(R)\t").split(" ")
+
+    # Some CPU brands have this weird character that fucks everything up
+    while "" in cpu_brand:
+        for i, e in enumerate(cpu_brand):
+            if e == "":
+                del cpu_brand[i]
+
+    new_cpu_brand = "".join(i + " " for i in cpu_brand).rstrip(" ")
 
     if not num_cpus:
         num_cpus = ""
