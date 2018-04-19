2c9ec133a90c2cef5a955e9e3cdfe4607a784c5d
Aareon Sullivan <askully13@gmail.com>
2018-04-19 11:44:59 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 from distutils.core import setup
 
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
 setup(
     name='lemongrab',
     version='0.1dev',
     author = "Aareon <https://github.com/Aareon>",
     packages=['lemongrab',],
     license='MIT License',
-    long_description=open('README.md').read(),
+    long_description=long_description,
     install_requires=[
     'distro==1.0.4',
     'psutil==5.2.2',
